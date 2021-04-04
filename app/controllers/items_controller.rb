67b2c328b06cdb9a34eb34b6b8833fae46cc379e
aloha0055 <76520202+aloha0055@users.noreply.github.com>
2021-04-04 10:15:01 +0900

    商品情報編集機能の実装１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,14 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :create, :update]
   before_action :authenticate_user!, except: [:show, :index]
+  before_action :move_to_index, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update]
 
   def index
     @items = Item.order('created_at DESC').includes(:user)
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def new
@@ -14,7 +16,6 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -22,9 +23,33 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+  end
+  
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:id,:name, :price, :category_id, :condition_id, :text, :shipping_charge_id, :shipping_area_id,:estimated_shipping_date_id, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @item.user
   end
 end
