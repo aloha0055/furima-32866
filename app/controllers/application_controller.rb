39a20deadcf04bf7351451920a769fe524c988a7
aloha0055 <76520202+aloha0055@users.noreply.github.com>
2021-04-19 18:53:02 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
-  #  以下、BASIC認証コード。他の機能を実装後にコメントアウト解除予定。WEB環境での実装確認済み。
-  # before_action :basic_auth
+  #  以下、BASIC認証コード。
+  before_action :basic_auth
 
-  # private
+  private
 
-  # def basic_auth
-  #   authenticate_or_request_with_http_basic do |username, password|
-  #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
-  #   end
-  # end
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
+    end
+  end
 
   # 以上、BASIC認証コード。
   before_action :configure_permitted_parameters, if: :devise_controller?
